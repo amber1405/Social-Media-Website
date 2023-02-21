1d7d898f55843a7f38feed97a868f8d8ee7a6a36
Amber Sinha <ambercs1405@gmail.com>
2023-02-21 11:04:57 +0530

    Setup Sign-In and Sign-Up Forms

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,26 @@ module.exports.profile = function(req, res){
         title: 'User Profile'
     })
 }
+
+// Render the Sign Up Page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codial|Sign Up"
+    })
+}
+
+// Render the Sign In Page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codial|Sign In"
+    })
+}
+
+// Get the Sign Up Data
+module.exports.create=function(req,res){
+    // Todo Later
+}
+// Sign In and Create Session
+module.exports.createSession=function(req,res){
+    // Todo Later
+}
